3ee7a1e287faee3441ed66da18f8a5d634bb30a2
Etan31 <tumbagatristanehron31@gmail.com>
2024-03-16 11:51:26 +0800

    added aqiText

--- assets/js/module.js
+++ assets/js/module.js
@@ -62,3 +62,26 @@ export const mps_to_kmh = mps =>{
    const mph = mps * 3600;
    return mph / 1000;
 }
+
+export const aqiText = {
+   1:{
+      level: "Good",
+      message: "Air quality is considered stisfactory, and air pollutin poses little or no risk."
+   },
+   2:{
+      level: "Fair",
+      message: "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."
+   },
+   3: {
+      level: "Moderate",
+      message: "Members of sensitive groups may experince health effects. The general public is not likely to be affected."
+   },
+   4: {
+      level: "Poor",
+      message: "Everyone may begin to experince health effects; members of sensitive groups may experience more serious health effects."
+   },
+   5: {
+      level: "Very Poor",
+      message: "Heath warning of emergency conditions. The entire population is more likely to be affected."
+   }
+} 
