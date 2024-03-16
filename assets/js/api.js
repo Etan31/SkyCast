1e7d79f0a4f80f44cb3addb8cc93dd446ad91241
Etan31 <tumbagatristanehron31@gmail.com>
2024-03-16 16:01:55 +0800

    Edited Index.html

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,30 +5,38 @@
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
 
-const api_key = "b8a84b455015781db98c293e744cbaf3"
+'use strict';
 
-export const  fetchData = function(URL, callback){
-   fetch(`${URL}&appid=${api_key}`)
-   .then(rex => res.json())
-   .then(data => callback(data));
+const api_key = "b8a84b455015781db98c293e744cbaf3";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
 }
 
-export const url = { 
-   //added a text lat and lon before the value
-   // https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=b8a84b455015781db98c293e744cbaf3
-   currentWeather(lat, lon) {
-      return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
-   },
-   forecast(lat, lon) {
-      return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
-   },
-   airPollution(lat, lon){
-      return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
-   },
-   reverseGeo(lat, lon){
-      return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
-   },
-   geo(query) {
-      return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
-   }
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+  },
+  /**
+   * @param {string} query Search query e.g.: "London", "New York"
+   */
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+  }
 }
--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,6 @@
 import { fetchData, url } from "./api.js";
 import * as module from "./module.js"
 
+ const addEventListener = function(elements, eventType, callback) {
+   for (const element of elements) element.addEventListener(eventType, callback);
+ }
