0cbcfb2afa5b9134f6ab013f9417ceecaece27d9
Etan31 <tumbagatristanehron31@gmail.com>
2024-03-16 15:45:31 +0800

    done with the modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,9 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js"
+
